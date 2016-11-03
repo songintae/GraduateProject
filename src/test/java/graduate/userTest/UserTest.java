eb879035203d1127186db35e663f54f5c8c1584e
namwoo <vangarang@naver.com>
2016-11-03 10:39:34 +0900

    test/../userTest 에 Json 뽑는거 넣어놔씀.

--- src/test/java/graduate/userTest/UserTest.java
+++ src/test/java/graduate/userTest/UserTest.java
@@ -14,6 +14,7 @@ import graduate.domain.TagWithCount;
 import graduate.domain.cluster.Attribute;
 import graduate.domain.cluster.Cluster;
 import graduate.userservice.UserService;
+import net.sf.json.JSONArray;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/applicationContext.xml")
@@ -34,14 +35,40 @@ public class UserTest {
 
 		// 이걸 json 형태로 바꿔서 지혜한테 넘겨야 함.
 		List<Attribute> attrs;
+		
+		JSONObject innerAttr = new JSONObject();
+		JSONObject inner;
+		JSONArray innerArray;
+		JSONArray clusterArray = new JSONArray();
+		JSONObject outer = new JSONObject();
+		JSONObject flare = new JSONObject();
+		JSONArray flareArray = new JSONArray();
+		
+		outer.put("name", "전주");
 		for (int i = 0; i < recommandedClusterList.size(); i++) {
 			attrs = userService.getAttributes(recommandedClusterList.get(i).getCluster_id());
 			System.out.println("cluster" + i + " : ");
+			innerArray = new JSONArray();
 			for (Attribute attr : attrs) {
+				innerAttr = new JSONObject();
+				innerAttr.put("name", attr.getTag());
+				innerAttr.put("size", attr.getCount());
 				if (attr.getCount() > 10)
 					System.out.print(attr.getTag() + ",");
+				innerArray.add(innerAttr);
 			}
-			System.out.println("");
+			inner = new JSONObject();
+			inner.put("name", "cluster " + i);
+			inner.put("children",innerArray);
+			clusterArray.add(inner);
 		}
+		outer.put("children", clusterArray);
+		
+		flareArray.add(outer);
+		flare.put("name", "flare");
+		flare.put("children", flareArray);
+		
+		System.out.println("");
+		System.out.println(outer.toJSONString());
 	}
 }
